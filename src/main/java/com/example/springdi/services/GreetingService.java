3d9e5bb8a07513faaaa3cff037c52cdd02f89b2c
ana <ana.aldea.c@gmail.com>
2023-06-19 14:18:09 +0300

    adding manual DI example

---
+++ src/main/java/com/example/springdi/services/GreetingService.java
@@ -0,0 +1,5 @@
+package com.example.springdi.services;
+
+public interface GreetingService {
+    String sayGreeting();
+}
---
+++ src/test/java/com/example/springdi/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,22 @@
+package com.example.springdi.controllers;
+
+import com.example.springdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SetterInjectedControllerTest {
+
+    SetterInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new SetterInjectedController();
+        controller.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(controller.getGreeting());
+
+    }
+}
