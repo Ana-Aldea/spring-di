5dab4db5af5adbdb6c63d5525b0c0da168c348db
ana <ana.aldea.c@gmail.com>
2023-06-19 20:36:43 +0300

    adding primary and profile examples

--- src/main/java/com/example/springdi/config/GreetingServiceConfig.java
+++ src/main/java/com/example/springdi/config/GreetingServiceConfig.java
@@ -1,13 +1,31 @@
 package com.example.springdi.config;
 
-import com.example.springdi.services.ConstructorGreetingService;
-import com.example.springdi.services.PropertyInjectedGreetingService;
-import com.example.springdi.services.SetterInjectedGreetingService;
+import com.example.springdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
+
+    @Profile({"ES", "default"})
+    @Bean("i18nService") //by default, Spring uses the method name unless you override it - which we did here
+    I18NSpanishService i18NSpanishService() {
+        return new I18NSpanishService();
+    }
+
+    @Profile("EN")
+    @Bean
+    I18nEnglishGreetingService i18nService() {
+        return new I18nEnglishGreetingService();
+    }
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService() {
+        return new PrimaryGreetingService();
+    }
 
     @Bean
     ConstructorGreetingService constructorGreetingService() {
