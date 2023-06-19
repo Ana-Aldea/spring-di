083dc4bf8ff9f1a3cc8091af0eb438310ed50faf
ana <ana.aldea.c@gmail.com>
2023-06-19 20:22:52 +0300

    adding java config example

---
+++ src/main/java/com/example/springdi/config/GreetingServiceConfig.java
@@ -0,0 +1,27 @@
+package com.example.springdi.config;
+
+import com.example.springdi.services.ConstructorGreetingService;
+import com.example.springdi.services.PropertyInjectedGreetingService;
+import com.example.springdi.services.SetterInjectedGreetingService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GreetingServiceConfig {
+
+    @Bean
+    ConstructorGreetingService constructorGreetingService() {
+        return new ConstructorGreetingService();
+    }
+
+    @Bean
+    PropertyInjectedGreetingService propertyInjectedGreetingService() {
+        return new PropertyInjectedGreetingService();
+    }
+
+    @Bean
+    SetterInjectedGreetingService setterInjectedGreetingService() {
+        return new SetterInjectedGreetingService();
+    }
+
+}
--- src/main/java/com/example/springdi/services/SetterInjectedGreetingService.java
+++ src/main/java/com/example/springdi/services/SetterInjectedGreetingService.java
@@ -1,9 +1,6 @@
 package com.example.springdi.services;
 
-import org.springframework.stereotype.Service;
-
-@Service
-public class SetterInjectedGreetingService implements GreetingService{
+public class SetterInjectedGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
         return "Hello World - Setter";
