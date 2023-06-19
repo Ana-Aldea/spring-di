3e1713462da88b618a0fee38813e5958b455271b
ana <ana.aldea.c@gmail.com>
2023-06-19 14:41:57 +0300

    adding examples for Spring Profiles

--- src/main/java/com/example/springdi/SpringDiApplication.java
+++ src/main/java/com/example/springdi/SpringDiApplication.java
@@ -1,9 +1,6 @@
 package com.example.springdi;
 
-import com.example.springdi.controllers.ConstructorInjectedController;
-import com.example.springdi.controllers.MyController;
-import com.example.springdi.controllers.PropertyInjectedController;
-import com.example.springdi.controllers.SetterInjectedController;
+import com.example.springdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,6 +11,9 @@ public class SpringDiApplication {
     public static void main(String[] args) {
 
         ApplicationContext ctx = SpringApplication.run(SpringDiApplication.class, args);
+
+        I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+        System.out.println(i18nController.sayHello());
 
         MyController myController = (MyController) ctx.getBean("myController");
 
